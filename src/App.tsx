8cc0f9c6969792e6afdd0b285b70225ae49ba9ee
Duke0404 <devansh0404@gmail.com>
2022-08-19 22:55:38 +0530

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const App = () => (
+	<div>
+		<h1>Hello World</h1>
+		<p>This is a simple React app</p>
+	</div>
+)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React stuff
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// Styles
+import "./index.css"
+
+// Components
+import App from "./App"
+
+// Root
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+
+// Render
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
