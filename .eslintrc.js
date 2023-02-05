3f55cd7c301645fe22af3c6d3c29f8c19a4c8f85
Duke0404 <01165826@pw.edu.pl>
2023-02-05 03:28:15 +0100

    Recoded from the ground up

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,22 @@ module.exports = {
 	env: {
 		browser: true,
 		node: true,
-		es2021: true
+		es2022: true
 	},
 	extends: [
 		"eslint:recommended",
 		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended"
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@nrwl/nx/react"
 	],
+
 	overrides: [],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		ecmaVersion: "latest",
 		sourceType: "module"
 	},
-	plugins: ["react", "@typescript-eslint"],
+	plugins: ["react", "@typescript-eslint", "@nrwl/nx"],
 	rules: {
 		"linebreak-style": ["error", "unix"],
 		quotes: ["error", "double"],
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,182 @@
-// React stuff
-// import useState
 import { useState } from "react"
+import Display from "./display"
 
-// Components
-// Import Display
-import Display from "./components/Display"
-// Import ButtonCluster
-import ButtonCluster from "./components/ButtonCluster"
+export default function App() {
+	const [equation, setEquation] = useState("")
+	const [history, setHistory] = useState<string[]>([])
 
-const App = () => {
-	// States
-	// State to hold the result of the calculation
-	const [display, setDisplay] = useState("")
+	function clear() {
+		setEquation("")
+	}
+
+	function append(input: string) {
+		setEquation(current => current + input)
+	}
+
+	function backspace() {
+		setEquation(current => current.slice(0, -1))
+	}
+
+	function addPoint() {
+		if (equation.length === 0) setEquation("0.")
+		else if (equation[equation.length - 1] === ".") return
+		else if (testNumber(equation[equation.length - 1])) setEquation(current => current + ".")
+		else setEquation(current => current + "0.")
+	}
+
+	const testNumber = (number: string) => RegExp(/^-?\d+\.?\d*$/).test(number)
+	const splitLastOccurrence = (input: string, substring: string) => [
+		input.slice(0, input.lastIndexOf(substring)),
+		input.slice(input.lastIndexOf(substring) + 1)
+	]
+
+	function calculate(input = equation): string {
+		if (testNumber(input)) return input
+		else if (input.includes("+")) {
+			const [a, b] = splitLastOccurrence(input, "+")
+			return (+calculate(a) + +calculate(b)).toString()
+		} else if (input.includes("-")) {
+			const [a, b] = splitLastOccurrence(input, "-")
+			return (+calculate(a) - +calculate(b)).toString()
+		} else if (input.includes("*")) {
+			const [a, b] = splitLastOccurrence(input, "*")
+			return (+calculate(a) * +calculate(b)).toString()
+		} else if (input.includes("/")) {
+			const [a, b] = splitLastOccurrence(input, "/")
+			return (+calculate(a) / +calculate(b)).toString()
+		} else return ""
+	}
+
+	const buttonStyle =
+		"flex items-center justify-center rounded-2xl text-2xl py-2  h-full shadow-lg hover:shadow-3xl dark:text-white"
 
 	return (
-		<main
-			className="
-				flex
-				flex-col
-				landscape:flex-row
-				grow
-			"
-		>
-			<Display display={display} />
-			<ButtonCluster setDisplay={setDisplay} />
-		</main>
+		<>
+			<Display value={equation} />
+
+			{/* <input
+				type="text"
+				value={equation}
+				onChange={e => setEquation(e.target.value)}
+			/> */}
+
+			<div
+				className="grid grid-rows-5 grid-cols-4 items-center
+				justify-center
+				gap-4
+				w-full"
+			>
+				<button
+					onClick={clear}
+					className={buttonStyle + " col-span-3 bg-blue-400 text-white"}
+				>
+					AC
+				</button>
+				<button
+					onClick={() => append("/")}
+					className={buttonStyle + " bg-pink-500 text-white"}
+				>
+					/
+				</button>
+
+				<button
+					onClick={() => append("7")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					7
+				</button>
+				<button
+					onClick={() => append("8")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					8
+				</button>
+				<button
+					onClick={() => append("9")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					9
+				</button>
+				<button
+					onClick={() => append("*")}
+					className={buttonStyle + " bg-pink-500 text-white"}
+				>
+					*
+				</button>
+
+				<button
+					onClick={() => append("4")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					4
+				</button>
+				<button
+					onClick={() => append("5")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					5
+				</button>
+				<button
+					onClick={() => append("6")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					6
+				</button>
+				<button
+					onClick={() => append("-")}
+					className={buttonStyle + " bg-pink-500 text-white"}
+				>
+					-
+				</button>
+
+				<button
+					onClick={() => append("1")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					1
+				</button>
+				<button
+					onClick={() => append("2")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					2
+				</button>
+				<button
+					onClick={() => append("3")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					3
+				</button>
+				<button
+					onClick={() => append("+")}
+					className={buttonStyle + " bg-pink-500 text-white"}
+				>
+					+
+				</button>
+
+				<button onClick={addPoint} className={buttonStyle + " bg-blue-400 text-white"}>.</button>
+				<button
+					onClick={() => append("0")}
+					className={buttonStyle + " bg-slate-100 dark:bg-zinc-900"}
+				>
+					0
+				</button>
+				<button
+					onClick={backspace}
+					className={buttonStyle + " bg-blue-400 text-white"}
+				>
+					C
+				</button>
+				<button
+					onClick={() => {
+						setHistory(current => [...current, equation])
+						setEquation(current => calculate(current))
+					}}
+					className={buttonStyle + " bg-indigo-700 text-white"}
+				>
+					=
+				</button>
+			</div>
+		</>
 	)
 }
-
-export default App
